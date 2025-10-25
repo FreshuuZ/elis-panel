c44a80f55d1e72038b618c82bbcd396d41cf71bd
FreshuuZ <125098226+FreshuuZ@users.noreply.github.com>
2025-10-25 22:02:52 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "elis-panel-v5";
+const CACHE_NAME = "elis-panel-v6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -37,4 +37,5 @@ self.addEventListener("fetch", (event) => {
   );
 
 });
+
 
